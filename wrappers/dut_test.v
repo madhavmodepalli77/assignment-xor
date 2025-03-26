748e0d3606da11dfe76def935c8a20baaa7d79a3
madhavmodepalli77 <63441831+madhavmodepalli77@users.noreply.github.com>
2025-03-25 23:05:21 -0400

    Update dut_test.v [skip ci]

--- wrappers/dut_test.v
+++ wrappers/dut_test.v
@@ -2,7 +2,7 @@ module dut_test(input wire a,b,
                 output wire y);
   dut exor DUT_call(.a(a), .b(b), .y(y));
 
-  initial begin
+  initial begin 
       $dumpfile("waves.vcd");
       $dumpvars;
     end
